7e8d7588fa4ad170ff26e028696323b695ddc95b
paoja <paojarobot2@gmail.com>
2018-01-19 16:04:36 +0700

    fucntioning student website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import './App.css';
-//import BasicExample from './BasicExample';
 import _ from 'lodash';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+//https://github.com/suthon-coe/pages
 
 const students = [
-  {id: 12,name: 'Marita', score: 23},
-  {id: 18,name: 'Dominic' ,score: 27},
-  {id: 20,name: 'Bosco' ,score: 22}
+  {id: '12', name: 'John Doe', score: 23},
+  {id: '18', name: 'Jenny Jason', score: 27},
+  {id: '20', name: 'Tim Brown', score: 22}
 ]
 
-const StudentLine = (props) => (
-  <div>{props.id} {props.name} = {props.score}</div>
+const Home = () => (<div>Home</div>)
+const Students = () => (
+  <div>
+    {
+      _.map(students, s => <StudentLink {...s} key={s.id}/>)
+    }
+    <Route path="/students/:id" component={StudentContainer}/>
+  </div>
+)
+const StudentContainer = ({match}) => {
+  let s = _.find(students, ['id', match.params.id])
+  return (
+    <StudentLine {...s} key={s.id}/>
+  )
+}
+const StudentLink = ({id, name}) => (
+  <div><Link to={`/students/${id}`}>{name}</Link></div>
+)
+const StudentLine = ({id, name, score}) => (
+  <div>{id} {name} = {score}</div>
 )
 class App extends Component {
   render() {
     return (
-     <div>
-      {
-        _.map(students, s => <StudentLine {...s}/>)
-      }
-     </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home}/>
+          <Route path="/students" component={Students}/> 
+        </div>       
+      </Router>
     );
   }
 }
