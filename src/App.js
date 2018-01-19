8d1b5d5e81418c5f2d3f50ca49089ca8c3b724c6
paoja <paojarobot2@gmail.com>
2018-01-19 16:02:56 +0700

    student id

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import BasicExample from './BasicExample';
+//import BasicExample from './BasicExample';
+import _ from 'lodash';
 
+const students = [
+  {id: 12,name: 'Marita', score: 23},
+  {id: 18,name: 'Dominic' ,score: 27},
+  {id: 20,name: 'Bosco' ,score: 22}
+]
+
+const StudentLine = (props) => (
+  <div>{props.id} {props.name} = {props.score}</div>
+)
 class App extends Component {
   render() {
     return (
-      <BasicExample/>
+     <div>
+      {
+        _.map(students, s => <StudentLine {...s}/>)
+      }
+     </div>
     );
   }
 }
